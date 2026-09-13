3f5cf4f79d30fc98ec491e7d7aa0402013c18c28
João Pedro Oliveira de Araujo <joaopedrooliveiradearaujo416@gmail.com>
2026-09-13 20:46:06 -0300

    feat: Changing a few things before deployment

--- API/Databases/Conn/trips.py
+++ API/Databases/Conn/trips.py
@@ -2,11 +2,11 @@ import os
 import psycopg2
 from dotenv import load_dotenv
 load_dotenv(encoding='utf-8')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
+DB_HOST = os.getenv('DB_HOST_TRIP')
+DB_PORT = os.getenv('DB_PORT_TRIP')
 DB_NAME = os.getenv('DB_NAME_TRIP')
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_USER = os.getenv('DB_USER_TRIP')
+DB_PASSWORD = os.getenv('DB_PASSWORD_TRIP')
 def connect_database_trip():
     try:
         conn = psycopg2.connect(
