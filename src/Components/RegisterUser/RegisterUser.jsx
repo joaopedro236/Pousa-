f87e88c16d544950cf66d56c5f0bd15fdc2b8ecb
João Pedro Oliveira de Araujo <joaopedrooliveiradearaujo416@gmail.com>
2026-09-14 21:55:43 -0300

    fix: removed reload

--- src/Components/RegisterUser/RegisterUser.jsx
+++ src/Components/RegisterUser/RegisterUser.jsx
@@ -26,7 +26,6 @@ export default function RegisterUser({ user, setUser, setLogin ,login, checkUser
             if (result?.Status) {
                 setUser(true)
                 await checkUser()
-                window.location.reload()
             }
             if (result?.Error) {
                 setError(true)
