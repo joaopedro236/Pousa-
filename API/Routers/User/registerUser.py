bc82b6534f2e91b3615a5d3723f2d968c8d51654
João Pedro Oliveira de Araujo <joaopedrooliveiradearaujo416@gmail.com>
2026-09-14 21:42:23 -0300

    fix: update samesite

--- API/Routers/User/registerUser.py
+++ API/Routers/User/registerUser.py
@@ -71,7 +71,7 @@ BLOCK
             value=session_token,
             httponly=True,
             max_age=60 * 60 * 24 * 7,
-            samesite="lax",
+            samesite="none",
             secure=True,
             path="/",
         )
