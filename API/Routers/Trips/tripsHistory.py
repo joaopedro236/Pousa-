b82315630745a42a22a7f9cd6c66f30e0d1e5990
João Pedro Oliveira de Araujo <joaopedrooliveiradearaujo416@gmail.com>
2026-09-15 21:28:23 -0300

    fix: update tripsHistory.py

--- API/Routers/Trips/tripsHistory.py
+++ API/Routers/Trips/tripsHistory.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, Request  
+from fastapi import APIRouter, Request
 from ...Databases.Conn.trips import connect_database_trip
 from ...Databases.Conn.users import connect_database
+
 router = APIRouter()
-@router.get('/getTripsHistory')
+
+
+@router.get("/getTripsHistory")
 def tripsHistory(request: Request):
     session_token = request.cookies.get("user_session_token")
     conn = None
@@ -10,15 +13,33 @@ def tripsHistory(request: Request):
     cursor = None
     cursorTrip = None
     try:
-        conn,cursor = connect_database()
+        conn, cursor = connect_database()
         connTrip, cursorTrip = connect_database_trip()
-        cursor.execute('select purchasedTrips from usersPousae where session_token = %s', (session_token,))
+        cursor.execute(
+            "select purchasedTrips from usersPousae where session_token = %s",
+            (session_token,),
+        )
         tripId = cursor.fetchone()[0]
-        cursorTrip.execute('select name, description,startdate, enddate,numberoftravelers, petsallowed, price , review from trips where id = ANY(%s)', (tripId,))
-        trip = [row[0] for row in cursorTrip.fetchall()]
-        return{'Status': True, 'Trip': trip}
+        cursorTrip.execute(
+            "select name, description,startdate, enddate,numberoftravelers, petsallowed, price , review from trips where id = ANY(%s)",
+            (tripId,),
+        )
+        trip = [
+            {
+                "name": row[0],
+                "description": row[1],
+                "startDate": row[2],
+                "endDate": row[3],
+                "numberOfTravelers": row[4],
+                "petsAllowed": row[5],
+                "price": row[6],
+                "review": row[7],
+            }
+            for row in cursorTrip.fetchall()
+        ]
+        return {"Status": True, "Trip": trip}
     except Exception as e:
-        return{'Status': False, 'Error': str(e)}
+        return {"Status": False, "Error": str(e)}
     finally:
         if cursor:
             cursor.close()
