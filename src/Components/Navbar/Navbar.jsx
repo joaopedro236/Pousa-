2ee4860de60f503d601a91aacd77cb6e9e997cdd
João Pedro Oliveira de Araujo <joaopedrooliveiradearaujo416@gmail.com>
2026-09-18 18:39:02 -0300

    fix: add console.log

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export default function Navbar({ ItemsNavbar, user, userData }) {
+    console.log(userData.tripsExists)
     const itemsNavbar = [
         { name: 'Explore', href: '#', value: 'explore' },
         { name: 'Stars', href: '#', value: 'stars' },
