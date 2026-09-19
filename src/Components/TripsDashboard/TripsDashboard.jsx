4df0c05ab683606ff09369e7b595c19d9cd68572
João Pedro Oliveira de Araujo <joaopedrooliveiradearaujo416@gmail.com>
2026-09-19 11:40:03 -0300

    feat: add cards (TripsDashboard)

--- src/Components/TripsDashboard/TripsDashboard.jsx
+++ src/Components/TripsDashboard/TripsDashboard.jsx
@@ -1,14 +1,44 @@
 import './tripDashboard.css'
-export default function TripsDashboard({itemsNavbar}){
-    return(
+import cards from './cards'
+import { useState, useEffect } from 'react'
+export default function TripsDashboard({ itemsNavbar }) {
+    const [json, setJson] = useState(null)
+    const requisition = async () => {
+        try {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/metrics`, {
+                method: 'GET',
+                credentials: 'include'
+            })
+            const data = await response.json()
+            if (data?.Error) {
+                console.error(data?.Error)
+            }
+            setJson(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    useEffect(()=>{
+        requisition()
+    },[])
+    return (
         <>
-        <section className={`tripsDashboard ${itemsNavbar == 'trip dashboard' ? 'd-flex' : 'd-none'}`}>
-            <header className='d-flex flex-column'>
-                <h1>Trip tracking dashboard </h1>
-                <p>Track revenues, operating profits, and traveler feedback in real time.</p>
-            </header>
-            <div className="cardsTrips"></div>
-        </section>
+            <section className={`tripsDashboard ${itemsNavbar == 'trip dashboard' ? 'd-flex' : 'd-none'}`}>
+                <header className='d-flex flex-column'>
+                    <h1>Trip tracking dashboard </h1>
+                    <p>Track revenues, operating profits, and traveler feedback in real time.</p>
+                </header>
+                <div className="cardsTrips ">
+                    {
+                        cards.map((cardsMap)=>(
+                            <div className="card" key={cardsMap.id}>
+                                <h2 className='fw-normal'>{cardsMap.title}</h2>
+                                <h1>{json[cardsMap?.jsonName]}</h1>
+                            </div>
+                        ))
+                    }
+                </div>
+            </section>
         </>
     )
 }
---
+++ src/Components/TripsDashboard/cards.js
@@ -0,0 +1,23 @@
+const cards = [
+    {
+        id:1,
+        title:'Total profit',
+        jsonName:'moneyObtained'
+    },
+    {
+        id:2,
+        title:"Trip numbers",
+        jsonName:'tripsObtained'
+    },
+    {
+        id:3,
+        title:'Average rating',
+        jsonName:'Review'
+    },
+    {
+        id:4,
+        title:'Comments received',
+        jsonName:"commentsCount"
+    }
+]
+export default cards
